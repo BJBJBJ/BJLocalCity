e29a2d6912e7860e8ccc7cd2a3419a981cabe684
zbj <mac@zbjdeMacBook-Pro.local>
2017-01-20 18:55:09 +0800

    mark

--- BJLocalCity/BJLocalCity/BJLocalCity/BJLocalCity.h
+++ BJLocalCity/BJLocalCity/BJLocalCity/BJLocalCity.h
@@ -7,28 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
-typedef void (^localAddressCallBack)(NSDictionary*address);//返回地址字典
-typedef void (^localCityCallBack)(NSString*city);//返回城市
-typedef void (^failure)(NSError*failure);//返回error
+
 @interface BJLocalCity : NSObject
+/** 单例*/
++(BJLocalCity*)bj_shared;
 /**
- *  单例
- *
- *  @return BJLocalCity
+ *  开启定位 返回地址数组
+ *  @param localAddressCompletion  定位回调成功返回地址数组
+ *  @param failure                 定位失败
  */
-+(BJLocalCity*)Shared;
++(void)bj_startLocalAddressSuccess:(void(^)(NSDictionary*address))localAddressCompletion failure:(void(^)(NSError*failure))failureBlock;
 /**
- *  开启定位
- *
- *  @param localAddressCallBack 定位回调成功返回地址数组
- *  @param failure              定位失败
+ *  开启定位 返回城市
+ *  @param localCityCompletion   定位回调成功返回城市
+ *  @param failure               定位失败
  */
-+(void)startLocalAddressSuccess:(localAddressCallBack)localAddressCallBack failure:(failure)failure;
-/**
- *  开启定位
- *
- *  @param localCallBack 定位回调成功返回城市
- *  @param failure       定位失败
- */
-+(void)startLocalCitySuccess:(localCityCallBack)localCallBack failure:(failure)failure;
++(void)bj_startLocalCitySuccess:(void(^)(NSString*city))localCityCompletion failure:(void(^)(NSError*failure))failureBlock;
 @end
--- BJLocalCity/BJLocalCity/BJLocalCity/BJLocalCity.m
+++ BJLocalCity/BJLocalCity/BJLocalCity/BJLocalCity.m
@@ -9,108 +9,140 @@
 #import "BJLocalCity.h"
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
+typedef void (^failureBlock)(NSError*failure);//返回error
+typedef void (^localAddressCompletion)(NSDictionary*address);//返回地址字典
+typedef void (^localCityCompletion)(NSString*city);//返回城市
 @interface BJLocalCity()<CLLocationManagerDelegate>
 
 @property (nonatomic , strong)CLLocationManager *locationManager;
-@property(nonatomic,copy)localCityCallBack localCallBack;
-@property(nonatomic,copy)failure failure;
-@property(nonatomic,copy)localAddressCallBack localAddressCallBack;
+@property(nonatomic,copy)localCityCompletion localCityCompletion;
+@property(nonatomic,copy)failureBlock failureBlock;
+@property(nonatomic,copy)localAddressCompletion localAddressCompletion;
 @end
 
 @implementation BJLocalCity
-static BJLocalCity* instance;
-
-+(BJLocalCity*)Shared{
-    if (!instance) {
+-(CLLocationManager *)locationManager{
+    if (!_locationManager) {
+       _locationManager=[[CLLocationManager alloc] init];
+        _locationManager.delegate=self;
+       _locationManager.desiredAccuracy=kCLLocationAccuracyBest;
+        CLLocationDistance distance=10.0;
+        _locationManager.distanceFilter=distance;
+  
+    }
+    return _locationManager;
+}
+static BJLocalCity* _instance=nil;
++(BJLocalCity*)bj_shared{
+    if (!_instance) {
         static dispatch_once_t onceToken;
         dispatch_once(&onceToken, ^{
-          instance = [[BJLocalCity alloc] init];
+          _instance = [[BJLocalCity alloc] init];
         });
     }
-    return instance;
+    return _instance;
 }
-+(void)startLocalAddressSuccess:(localAddressCallBack)localAddressCallBack failure:(failure)failure{
-    BJLocalCity *local=[BJLocalCity Shared];
-    [local startLocation];
-    local.localAddressCallBack=localAddressCallBack;
-    local.failure=failure;
-}
-+(void)startLocalCitySuccess:(localCityCallBack)localCallBack failure:(failure)failure{
-    BJLocalCity *local=[BJLocalCity Shared];
-    [local startLocation];
-    local.localCallBack=localCallBack;
-    local.failure=failure;
++(void)bj_startLocalAddressSuccess:(void(^)(NSDictionary*address))localAddressCompletion failure:(void(^)(NSError*failure))failureBlock{
+    BJLocalCity *local=[BJLocalCity bj_shared];
+    [local bj_startLocation];
+    local.localAddressCompletion=localAddressCompletion;
+    local.failureBlock=failureBlock;
 }
 
-/**
- *  开始定位
- */
--(void)startLocation{
-    self.locationManager=[[CLLocationManager alloc] init];
-    self.locationManager.delegate=self;
-    self.locationManager.desiredAccuracy=kCLLocationAccuracyBest;
-    CLLocationDistance distance=10.0;
-    self.locationManager .distanceFilter=distance;
++(void)bj_startLocalCitySuccess:(void(^)(NSString*city))localCityCompletion failure:(void(^)(NSError*failure))failureBlock{
+    BJLocalCity *local=[BJLocalCity bj_shared];
+    [local bj_startLocation];
+    local.localCityCompletion=localCityCompletion;
+    local.failureBlock=failureBlock;
+}
 
-    if (![CLLocationManager locationServicesEnabled]){//定位不可用
-        NSLog(@"--%@--定位服务不可用",[self class]);
+/** 开始定位*/
+-(void)bj_startLocation{
+    
+    //定位服务不可用
+    if (![CLLocationManager locationServicesEnabled]){
+        if (self.failureBlock) {
+            NSError *error=[NSError errorWithDomain:@"定位服务不可用" code:-1 userInfo:@{@"定位服务":@"无法使用"}];
+            self.failureBlock(error);
+        }
         return;
     }
     
-    if ([[[UIDevice currentDevice] systemVersion] doubleValue]<8.0){//8.0以下
-        
-       [self.locationManager startUpdatingLocation];//开启定位
+    //iOS 8.0以下开启定位
+    if ([[[UIDevice currentDevice] systemVersion] doubleValue]<8.0){
+        //开启定位
+       [self.locationManager startUpdatingLocation];
         return;
     }
-    //8.0以上
-    if ([CLLocationManager authorizationStatus]==kCLAuthorizationStatusNotDetermined){//未确定授权状态
-        
-        [self.locationManager requestAlwaysAuthorization];//设置授权状态
+    //iOS 8.0以上开启定位
+    //未确定授权状态
+    if ([CLLocationManager authorizationStatus]==kCLAuthorizationStatusNotDetermined){
+        //设置授权状态
+        [self.locationManager requestAlwaysAuthorization];
+        //开启定位
         [self.locationManager startUpdatingLocation];
         
         return;
     }
+    //已获取授权 直接开启定位
    if ([CLLocationManager authorizationStatus]==kCLAuthorizationStatusAuthorizedAlways||[CLLocationManager authorizationStatus]==kCLAuthorizationStatusAuthorizedWhenInUse){
         
         [self.locationManager startUpdatingLocation];
        return;
     }
 
+    //定位权限被用户禁用
+    if ([CLLocationManager authorizationStatus]==kCLAuthorizationStatusDenied) {
+        if (self.failureBlock) {
+            NSError *error=[NSError errorWithDomain:@"未授权定位" code:-2 userInfo:@{@"定位权限":@"禁用"}];
+            self.failureBlock(error);
+  
+        }
+     }
+    
 }
-#pragma mark----定位代理-经纬度反编码----
-
+#pragma mark----CLLocationManagerDelegate----
+#pragma mark 经纬度反编码
+//此回调开启定位后会不断调用
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
-    [self.locationManager stopUpdatingLocation];//结束定位
-    //反编码
-    CLGeocoder *Geocoder=[[CLGeocoder alloc] init];
+    
+    NSLog(@"aaaaa");
+    //1.结束定位
+    [self.locationManager stopUpdatingLocation];
+    //2.取坐标
     CLLocation * location=[locations lastObject];
+    //3.反编码
+    CLGeocoder *Geocoder=[[CLGeocoder alloc] init];
     CLLocation *newLocation = [[CLLocation alloc] initWithLatitude:location.coordinate.latitude longitude:location.coordinate.longitude];
     [Geocoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error)
      {
-            if (error&&self.failure) {
-                self.failure(error);
-                return ;
-             }
+         //编码错误
+        if (error&&self.failureBlock) {
+            self.failureBlock(error);
+            return ;
+         }
 
-         //传出地址
+         //1.传出地址
         CLPlacemark *placemark=[placemarks firstObject];
          
-         if (placemark.addressDictionary&&self.localAddressCallBack) {
-             self.localAddressCallBack(placemark.addressDictionary);
+         if (placemark.addressDictionary&&self.localAddressCompletion) {
+             self.localAddressCompletion(placemark.addressDictionary);
              return;
          }
-         
-         if (placemark.addressDictionary[@"City"]&&self.localCallBack){
-           self.localCallBack(placemark.addressDictionary[@"City"]);
+         //2.传出城市
+         if (placemark.addressDictionary[@"City"]&&self.localCityCompletion){
+           self.localCityCompletion(placemark.addressDictionary[@"City"]);
                  return;
             }
      }];
 
 }
+
+#pragma mark 定位失败回调
 -(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
     
-    if (self.failure) {
-        self.failure(error);
+    if (self.failureBlock) {
+        self.failureBlock(error);
     }
 }
 @end
